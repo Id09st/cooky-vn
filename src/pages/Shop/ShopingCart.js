5e9f71c8e6c754f72ec2bc36398e3c8280ee2450
Tran Minh Thien <nhoktmt2002@gmail.com>
2023-06-15 02:07:24 +0700

    trang ShoppingCart mơi

--- src/pages/Shop/ShopingCart.js
+++ src/pages/Shop/ShopingCart.js
@@ -1,159 +1,253 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import {
+  Table,
+  TableContainer,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  IconButton,
+  Container,
+  useMediaQuery,
+  Card,
+  CardContent,
+  Button,
+  Box,
+  Typography,
+  Breadcrumbs,
+  TextField,
+} from '@mui/material';
+import { Delete } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 
-export default function ShopingCart() {
-  useEffect(() => {
-    const setBgImages = () => {
-      const elements = document.getElementsByClassName('set-bg');
-      Array.from(elements).forEach((element) => {
-        const bg = element.dataset.setbg;
-        element.style.backgroundImage = `url(${bg})`;
-      });
-    };
-    setBgImages();
-  }, []);
+const ShoppingCart = () => {
+  const [cartItems, setCartItems] = useState([
+    { id: 1, name: 'Product 1', price: 10, quantity: 2, image: '/img/cart/cart-1.jpg' },
+    { id: 2, name: 'Product 2', price: 15, quantity: 3, image: '/img/cart/cart-2.jpg' },
+    { id: 3, name: 'Product 3', price: 20, quantity: 1, image: '/img/cart/cart-3.jpg' },
+  ]);
+
+  const [couponCode, setCouponCode] = useState('');
+  const [couponAmount, setCouponAmount] = useState(0);
+
+  const handleQuantityChange = (itemId, amount) => {
+    const updatedCartItems = cartItems.map((item) => {
+      if (item.id === itemId) {
+        return {
+          ...item,
+          quantity: item.quantity + amount,
+        };
+      }
+      return item;
+    });
+
+    setCartItems(updatedCartItems);
+  };
+
+  const handleDelete = (itemId) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== itemId);
+    setCartItems(updatedCartItems);
+  };
+
+  const handleApplyCoupon = () => {
+    // Placeholder logic to check and apply coupon code
+    if (couponCode === 'SAVE20') {
+      setCouponAmount(20); // Assuming a fixed discount of $20
+    } else {
+      setCouponAmount(0);
+    }
+  };
+
+  const isMobile = useMediaQuery('(max-width: 601px)');
+
+  const subtotal = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  const total = subtotal - couponAmount;
+
   return (
-    <>
-      <>
-        {/* Breadcrumb Section Begin */}
-        <section className="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12 text-center">
-                <div className="breadcrumb__text">
-                  <h2>Shopping Cart</h2>
-                  <div className="breadcrumb__option">
-                    <Link to="/">Home</Link>
-                    <span>Shopping Cart</span>
+    <Container maxWidth="lg" style={{ padding: '20px' }}>
+      {isMobile ? (
+        <div>
+          {cartItems.map((item) => (
+            <Card key={item.id} style={{ marginBottom: '10px' }}>
+              <CardContent>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <div>
+                    <img src={item.image} alt={item.name} style={{ width: '100px', height: '100px' }} />
+                  </div>
+                  <div style={{ marginLeft: '5px' }}>
+                    <div>
+                      Quantity:
+                      <IconButton onClick={() => handleQuantityChange(item.id, -1)}>-</IconButton>
+                      {item.quantity}
+                      <IconButton onClick={() => handleQuantityChange(item.id, 1)}>+</IconButton>
+                    </div>
+                    <div>Total: ${item.price * item.quantity}</div>
                   </div>
                 </div>
-              </div>
-            </div>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <div style={{ flex: '1' }}>
+                    <div>{item.name}</div>
+                    <div>Price: ${item.price}</div>
+                  </div>
+                  <IconButton onClick={() => handleDelete(item.id)}>
+                    <Delete />
+                  </IconButton>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+          <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ backgroundColor: 'var(--primary-color)', color: 'var(--white-color)' }}
+            >
+              Continue Shopping
+            </Button>
           </div>
-        </section>
-        {/* Breadcrumb Section End */}
-        {/* Shoping Cart Section Begin */}
-        <section className="shoping-cart spad">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="shoping__cart__table">
-                  <table>
-                    <thead>
-                      <tr>
-                        <th className="shoping__product">Products</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th>Total</th>
-                        <th />
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td className="shoping__cart__item">
-                          <img src="img/cart/cart-1.jpg" alt="" />
-                          <h5>Vegetable’s Package</h5>
-                        </td>
-                        <td className="shoping__cart__price">$55.00</td>
-                        <td className="shoping__cart__quantity">
-                          <div className="quantity">
-                            <div className="pro-qty">
-                              <input type="text" defaultValue={1} />
-                            </div>
-                          </div>
-                        </td>
-                        <td className="shoping__cart__total">$110.00</td>
-                        <td className="shoping__cart__item__close">
-                          <span className="icon_close" />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="shoping__cart__item">
-                          <img src="img/cart/cart-2.jpg" alt="" />
-                          <h5>Fresh Garden Vegetable</h5>
-                        </td>
-                        <td className="shoping__cart__price">$39.00</td>
-                        <td className="shoping__cart__quantity">
-                          <div className="quantity">
-                            <div className="pro-qty">
-                              <input type="text" defaultValue={1} />
-                            </div>
-                          </div>
-                        </td>
-                        <td className="shoping__cart__total">$39.99</td>
-                        <td className="shoping__cart__item__close">
-                          <span className="icon_close" />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="shoping__cart__item">
-                          <img src="img/cart/cart-3.jpg" alt="" />
-                          <h5>NiceCookc Bananas</h5>
-                        </td>
-                        <td className="shoping__cart__price">$69.00</td>
-                        <td className="shoping__cart__quantity">
-                          <div className="quantity">
-                            <div className="pro-qty">
-                              <input type="text" defaultValue={1} />
-                            </div>
-                          </div>
-                        </td>
-                        <td className="shoping__cart__total">$69.99</td>
-                        <td className="shoping__cart__item__close">
-                          <span className="icon_close" />
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="shoping__cart__btns">
-                  <a href="#" className="primary-btn cart-btn">
-                    CONTINUE SHOPPING
-                  </a>
-                  <a href="#" className="primary-btn cart-btn cart-btn-right">
-                    <span className="icon_loading" />
-                    Upadate Cart
-                  </a>
-                </div>
-              </div>
-              <div className="col-lg-6">
-                <div className="shoping__continue">
-                  <div className="shoping__discount">
-                    <h5>Discount Codes</h5>
-                    <form action="#">
-                      <input type="text" placeholder="Enter your coupon code" />
-                      <button type="submit" className="site-btn">
-                        APPLY COUPON
-                      </button>
-                    </form>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-6">
-                <div className="shoping__checkout">
-                  <h5>Cart Total</h5>
-                  <ul>
-                    <li>
-                      Subtotal <span>$454.98</span>
-                    </li>
-                    <li>
-                      Total <span>$454.98</span>
-                    </li>
-                  </ul>
-                  <Link to="/checkout" className="primary-btn">
-                    PROCEED TO CHECKOUT
+          <div style={{ marginTop: '20px' }}>
+            <TextField
+              label="Enter your coupon code"
+              variant="outlined"
+              size="small"
+              value={couponCode}
+              onChange={(e) => setCouponCode(e.target.value)}
+            />
+            <Button
+              variant="contained"
+              onClick={handleApplyCoupon}
+              style={{ backgroundColor: 'var(--primary-color)', color: 'var(--white-color)' }}
+            >
+              APPLY COUPON
+            </Button>
+          </div>
+
+          <div style={{ marginTop: '20px' }}>
+            <div>Subtotal: ${subtotal}</div>
+            <div>Coupon: ${couponAmount}</div>
+            <div>Total: ${total}</div>
+            <Button
+              variant="contained"
+              style={{ backgroundColor: 'var(--primary-color)', color: 'var(--white-color)' }}
+            >
+              PROCEED TO CHECKOUT
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Box
+            component="section"
+            sx={{
+              backgroundImage: 'url(img/breadcrumb.jpg)',
+              backgroundSize: 'cover',
+              py: 4,
+            }}
+          >
+            <Container maxWidth="lg">
+              <Box textAlign="center">
+                <Typography variant="h2" style={{ color: 'var(--white-color)' }}>
+                  Shopping Cart
+                </Typography>
+                <Breadcrumbs
+                  aria-label="breadcrumb"
+                  separator="›"
+                  style={{ color: 'var(--white-color)' }}
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  <Link underline="hover" to="/">
+                    <Typography style={{ color: 'var(--white-color)' }}>Home</Typography>
                   </Link>
-                </div>
+                  <Typography style={{ color: 'var(--white-color)' }} variant="body1">
+                    Shopping Cart
+                  </Typography>
+                </Breadcrumbs>
+              </Box>
+            </Container>
+          </Box>
+
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Products</TableCell>
+                  <TableCell>Price</TableCell>
+                  <TableCell>Quantity</TableCell>
+                  <TableCell>Total</TableCell>
+                  <TableCell></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {cartItems.map((item) => (
+                  <TableRow key={item.id}>
+                    <TableCell>
+                      <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <img src={item.image} alt={item.name} style={{ width: '100px', height: '100px' }} />
+                        <span style={{ marginLeft: '10px' }}>{item.name}</span>
+                      </div>
+                    </TableCell>
+                    <TableCell>${item.price}</TableCell>
+                    <TableCell>
+                      <IconButton onClick={() => handleQuantityChange(item.id, -1)}>-</IconButton>
+                      {item.quantity}
+                      <IconButton onClick={() => handleQuantityChange(item.id, 1)}>+</IconButton>
+                    </TableCell>
+                    <TableCell>${item.price * item.quantity}</TableCell>
+                    <TableCell>
+                      <IconButton onClick={() => handleDelete(item.id)}>
+                        <Delete />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px' }}>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: 'var(--primary-color)', color: 'var(--white-color)' }}
+              >
+                Continue Shopping
+              </Button>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <TextField
+                  label="Enter your coupon code"
+                  variant="outlined"
+                  value={couponCode}
+                  onChange={(e) => setCouponCode(e.target.value)}
+                  style={{ marginRight: '10px' }}
+                />
+                <Button
+                  variant="contained"
+                  onClick={handleApplyCoupon}
+                  style={{ backgroundColor: 'var(--primary-color)', color: 'var(--white-color)' }}
+                >
+                  APPLY COUPON
+                </Button>
               </div>
             </div>
-          </div>
-        </section>
-        {/* Shoping Cart Section End */}
-      </>
-    </>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
+              <div>
+                <div>Subtotal: ${subtotal}</div>
+                <div>Coupon: ${couponAmount}</div>
+                <div>Total: ${total}</div>
+                <Button
+                  variant="contained"
+                  style={{ backgroundColor: 'var(--primary-color)', color: 'var(--white-color)' }}
+                >
+                  PROCEED TO CHECKOUT
+                </Button>
+              </div>
+            </div>
+          </TableContainer>
+        </>
+      )}
+    </Container>
   );
-}
+};
+
+export default ShoppingCart;
