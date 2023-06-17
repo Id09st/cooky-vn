d1a5d4f4af83fdeb9fc0446073ee89101dec5aa8
heheboizzzz <mrtuan154@gmail.com>
2023-06-17 17:22:01 +0700

    sua header

--- src/components/Layouts/DefaultLayout/Header/Header.js
+++ src/components/Layouts/DefaultLayout/Header/Header.js
@@ -1,68 +1,91 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '~/sass/_header.scss';
-import '~/sass/_responsive.scss';
-import Badge from '@mui/material/Badge';
+import * as React from 'react';
+import { useState } from 'react';
 import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { Link } from 'react-router-dom';
 import logo from '~/assets/images/logo.png';
-import { FavoriteBorderRounded, ShoppingCartOutlined } from '@mui/icons-material';
-import { IconButton, InputBase, Dialog, DialogContent, Button } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import CloseIcon from '@mui/icons-material/Close';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import { Button, Dialog, DialogContent } from '@mui/material';
 import LoginForm from '~/pages/Login/LoginForm';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import CloseIcon from '@mui/icons-material/Close';
+import LoginIcon from '@mui/icons-material/Login';
+import PersonIcon from '@mui/icons-material/Person';
+import { FavoriteBorderRounded, ShoppingCartOutlined } from '@mui/icons-material';
+import CallOutlinedIcon from '@mui/icons-material/CallOutlined';
 
-export default function Header() {
-  const StyledBadge = styled(Badge)(({ theme }) => ({
-    '& .MuiBadge-badge': {
-      right: -3,
-      top: 13,
-      border: `2px solid ${theme.palette.background.paper}`,
-      padding: '0 4px',
-    },
-  }));
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
 
-  const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
     width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
+    [theme.breakpoints.up('md')]: {
+      width: '80ch',
     },
-  }));
+  },
+}));
 
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
+export default function PrimarySearchAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
-    },
-  }));
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
 
   const [open, setOpen] = useState(false);
 
@@ -73,76 +96,172 @@ export default function Header() {
   const handleClose = () => {
     setOpen(false);
   };
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>
+        <PersonIcon />
+        User
+      </MenuItem>
+      <MenuItem onClick={handleMenuClose}>
+        <Button onClick={handleOpen} sx={{ color: 'black' }}>
+          <LoginIcon />
+          Login
+        </Button>
+        <Dialog open={open} onClose={handleClose}>
+          <IconButton
+            edge="end"
+            color="inherit"
+            onClick={handleClose}
+            aria-label="close"
+            sx={{ position: 'absolute', right: 8, top: 8 }}
+          >
+            <CloseIcon />
+          </IconButton>
+          <DialogContent>
+            <LoginForm />
+          </DialogContent>
+        </Dialog>
+      </MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new favorite Foood" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <FavoriteBorderRounded />
+          </Badge>
+        </IconButton>
+      </MenuItem>
+      <MenuItem>
+        <IconButton size="large" aria-label="show 17 new shoping cart" color="inherit">
+          <Link to="/shoping-cart" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Badge badgeContent={17} color="error">
+              <ShoppingCartOutlined sx={{ color: 'black' }} />
+            </Badge>
+          </Link>
+        </IconButton>
+      </MenuItem>
+      <MenuItem>
+        <IconButton size="large" aria-label="Contact" color="inherit">
+          <Link to="/contact" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <CallOutlinedIcon sx={{ color: 'black' }} />
+          </Link>
+        </IconButton>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
-    <>
-      <header className="header">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-3">
-              <div className="header__logo">
-                <Link to="/">
-                  <img src={logo} alt="/" />
-                </Link>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <Search style={{ marginTop: '25px' }}>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
-              </Search>
-            </div>
-            <div className="col-lg-3">
-              <div className="header__cart">
-                <ul>
-                  <li>
-                    <IconButton aria-label="cart">
-                      <StyledBadge badgeContent={4} color="secondary">
-                        <Link to='#'>
-                          <FavoriteBorderRounded />
-                        </Link>
-                      </StyledBadge>
-                    </IconButton>
-                  </li>
-                  <li>
-                    <IconButton aria-label="cart">
-                      <StyledBadge badgeContent={4} color="secondary">
-                        <Link to="/shoping-cart">
-                          <ShoppingCartOutlined />
-                        </Link>
-                      </StyledBadge>
-                    </IconButton>
-                  </li>
-                </ul>
-                <div className="header__cart__price">
-                  item: <span>$150.00</span>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-2">
-              <Button style={{ marginTop: '15px' }} aria-label="Example" onClick={handleOpen} sx={{ fontSize: 40 }}>
-                <AccountCircleIcon fontSize="lagre" />
-              </Button>
-              <Dialog open={open} onClose={handleClose}>
-                <IconButton
-                  edge="end"
-                  color="inherit"
-                  onClick={handleClose}
-                  aria-label="close"
-                  sx={{ position: 'absolute', right: 8, top: 8 }}
-                >
-                  <CloseIcon />
-                </IconButton>
-                <DialogContent>
-                  <LoginForm />
-                </DialogContent>
-              </Dialog>
-            </div>
-          </div>
-        </div>
-      </header>
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: '#FFA500' }}>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            style={{ marginLeft: '165px' }}
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            <Link to="/">
+              <img src={logo} alt="/" />
+            </Link>
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box style={{ marginRight: '167px' }} sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton size="large" aria-label="show 4 new favorite Foood" color="inherit">
+              <Badge badgeContent={4} color="error">
+                <FavoriteBorderRounded sx={{ color: 'white' }} />
+              </Badge>
+            </IconButton>
+            <IconButton size="large" aria-label="show 17 new shoping cart" color="inherit">
+              <Link to="/shoping-cart">
+                <Badge badgeContent={17} color="error">
+                  <ShoppingCartOutlined sx={{ color: 'white' }} />
+                </Badge>
+              </Link>
+            </IconButton>
+            <IconButton size="large" aria-label="Contact" color="inherit">
+              <Link to="/contact">
+                <CallOutlinedIcon sx={{ color: 'white' }} />
+              </Link>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
   );
 }
